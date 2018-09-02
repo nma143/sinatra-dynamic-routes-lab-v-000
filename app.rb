e58e7b8529e3832b9abd25cceac7d978b993586b
Nikki A <nikki.apostolakis@gmail.com>
2018-09-02 19:29:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,11 +20,8 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    x=@num
-    x.times do
-      "hi"
-      binding.pry
-    end
+    "#{@phrase}"
+    "#{@phrase}"
     "#{@phrase}"
   end
 
