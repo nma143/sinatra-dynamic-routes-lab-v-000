f89e3f77632ed87784556af0974226032e287c38
Nikki A <nikki.apostolakis@gmail.com>
2018-09-02 19:21:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,28 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  # Accepts a name and renders the name backwards.
+  get '/reversename/:name' do
+    @name= params[:name]
+    @reversename = @name.reverse
+    "#{@reversename}"
+  end
+
+  # Accepts a number and returns the square of that number
+  get '/square/:number' do
+    @num= params[:number].to_i
+    "#{@num*@num}"
+  end
+
+  # Accepts a number and a phrase and returns that phrase in a string the number of times given
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    count = 0
+    while count <= @num
+      count = count +1
+      "#{@phrase}"
+     end
+  end
+
 end
